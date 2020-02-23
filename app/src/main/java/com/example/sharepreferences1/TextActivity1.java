a180519d8255f13522021adb6cf6a3a2f80ce7e4
chen <651932353@qq.com>
2020-02-24 00:29:48 +0800

    模拟网络服务器，设置服务器内部XML数据内容，发送HTTP请求并解析出XML数据内容，打印Log.d显示 重点学习构造Reader和XmlPullParser使用的方法。根据解析节点名称赋予具体详细内容，继续循环下一个id获取内容直到出现End_TAG。

--- app/src/main/java/com/example/sharepreferences1/TextActivity1.java
+++ app/src/main/java/com/example/sharepreferences1/TextActivity1.java
@@ -2,6 +2,7 @@ package com.example.sharepreferences1;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -9,7 +10,11 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+
 import java.io.IOException;
+import java.io.StringReader;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -19,7 +24,7 @@ public class TextActivity1 extends AppCompatActivity implements View.OnClickList
     private EditText editText2;
     private EditText editText3;
     private TextView httpText;
-
+    private static final String TAG = "TextActivity1";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,11 +55,14 @@ public class TextActivity1 extends AppCompatActivity implements View.OnClickList
                     public void run() {
 
                         try {
+//                            发送请求均需要设定Client和Request变量对象
                             OkHttpClient okHttpClient = new OkHttpClient();
                             Request request = new Request.Builder()
-                                    .url("http://www.bing.com")
+                                    .url("http://10.0.2.2/get_data.xml")
                                     .build();
+//                            根据请求码发送，返回Response对象就是获取的信息，execute需要抛出异常代码
                             Response response = okHttpClient.newCall(request).execute();
+//                            将数据转换成String形式
                             String responseData = response.body().string();
                             showResponse(responseData);
                         } catch (IOException e) {
@@ -62,16 +70,60 @@ public class TextActivity1 extends AppCompatActivity implements View.OnClickList
                         }
                     }
                 }).start();
+                break;
 
+            default:
+                break;
         }
     }
 
     private void showResponse(final String responseData) {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                httpText.setText(responseData);
+        try {
+//            构造出XmlPullParser实例
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            XmlPullParser xmlPullParser = factory.newPullParser();
+//            将服务器返回的XML数据设置进去xmlPullParser就可以开始解析，数据类型是String
+            xmlPullParser.setInput(new StringReader(responseData));
+//            重点：根据XmlPullParser生成eventType，根据事件不同处理
+            int eventType = xmlPullParser.getEventType();
+            String id = "";
+            String name = "";
+            String version = "";
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+//               获得当前事件的名称， .getName返回节点名称，String类型
+                String nodeName = xmlPullParser.getName();
+                switch (eventType) {
+                    case XmlPullParser.START_TAG: {
+//               发现节点名等于id、name或version，就调用nextText() 方法来获取节点内具体的内容
+                        if ("id".equals(nodeName)) {
+                            id = xmlPullParser.nextText();
+                        } else if ("name".equals(nodeName)) {
+                            name = xmlPullParser.nextText();
+                        } else if ("version".equals(nodeName)) {
+                            version = xmlPullParser.nextText();
+                        }
+                        break;
+                    }
+                    case XmlPullParser.END_TAG: {
+                        if ("app".equals(nodeName)) {
+                            Log.d(TAG, "id is " + id);
+                            Log.d(TAG, "name is " + name);
+                            Log.d(TAG, "version is " + version);
+
+                        }
+                        break;
+
+                    }
+                    default:
+                        break;
+                }
+
+                eventType = xmlPullParser.next();
             }
-        });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
+
 }
